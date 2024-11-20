189ca62a5978fc1a21b515629bca7987953fa20e
MisterG <g.haegerbaeumer@gmx.de>
2024-11-20 10:13:23 +0100

    Added Interaction Stuff

---
+++ Assets/Scripts/Controller/UI/InteractUIController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Controller.UI
+{
+    public class InteractUIController : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] [Tooltip("The mask for the Circle Effect")]
+        private Image circle;
+
+        public void SetToAmount(float value) => circle.fillAmount = value;
+    }
+}
---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,28 @@
+using Controller.UI;
+using UnityEngine;
+
+namespace Manager
+{
+    public class UIManager : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] [Tooltip("A Reference to the Interaction UI element.")]
+        private InteractUIController interactUIController;
+        
+        // Publics
+        public static UIManager Instance;
+        public InteractUIController InteractionUI => interactUIController;
+        
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Debug.LogWarning("More than one UI Manager in scene.");
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+    }
+}
