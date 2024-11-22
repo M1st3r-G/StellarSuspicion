78a79da7c2565afaab2044d43acdf28953f5dabf
M1st3r-G <g.haegerbaeumer@gmx.de>
2024-11-22 13:24:59 +0100

    Fixed Outline Stuff

---
+++ Assets/Scripts/Controller/Actors/Interactable/GlowOnHover.cs
@@ -0,0 +1,29 @@
+﻿using QuickOutline;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Controller.Actors.Interactable
+{
+    [RequireComponent(typeof(Outline))]
+    public class GlowOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        private Outline _outline;
+
+        private void Awake()
+        {
+            _outline = GetComponent<Outline>();
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            Debug.LogWarning("Entered");
+            _outline.enabled = true;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Debug.LogWarning("Exit");
+            _outline.enabled = false;
+        }
+    }
+}
